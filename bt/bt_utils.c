6ba45e1f50195e0c6d7e3f059d8d4d59e9fde6e3
kivr <learsi_naitsirk@hotmail.com>
2018-02-12 22:55:52 -0600

    Adding connect to bt_utils

--- bt/bt_utils.c
+++ bt/bt_utils.c
@@ -1,4 +1,5 @@
 #include <sys/socket.h>
+#include <unistd.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/l2cap.h>
 
@@ -25,4 +26,28 @@ int bt_wait_for_connection_on_psm(int *serverSocket, int psm)
 	client = accept(*serverSocket, NULL, NULL);
 
 	return client;
+}
+
+int bt_connect_to_psm(const char *s_address, int psm)
+{
+    struct sockaddr_l2 address;
+    int s, status;
+
+    memset(&address, 0, sizeof(address));
+
+    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_L2CAP);
+
+    address.l2_family = AF_BLUETOOTH;
+    address.l2_psm = 17;
+    str2ba(s_address, &address.l2_bdaddr);
+
+    status = connect(s, (struct sockaddr*)&address, sizeof(address));
+
+    if (status != 0)
+    {
+        close(s);
+        s = -1;
+    }
+
+    return s;
 }
--- bt/bt_utils.h
+++ bt/bt_utils.h
@@ -1,1 +1,2 @@
 int bt_wait_for_connection_on_psm(int *serverSocket, int psm);
+int bt_connect_to_psm(const char *address, int psm);
