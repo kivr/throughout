161c36337aad83a0d701d83541e1b7b4aa743475
kivr <learsi_naitsirk@hotmail.com>
2018-02-17 23:23:22 -0600

    Adding context to throughout program

--- throughout.c
+++ throughout.c
@@ -10,92 +10,93 @@
 #include "bt/bt_hid.h"
 #include "bt/utils/bt_utils.h"
 
-#define CLIENT0_ADDRESS "60:BE:B5:30:61:AB"
-#define CLIENT1_ADDRESS "b8:8a:60:6a:68:d6"
-#define PREFIX "\xa1\x12"
+#define USB_PREFIX "\xa1\x12"
+#define USB_BUFFER_SIZE (USB_INPUT_SIZE + sizeof(USB_PREFIX) - 1)
 
 #define USB_INPUT_SIZE 8
 #define BT_INPUT_SIZE 64
-#define USB_BUFFER_SIZE (USB_INPUT_SIZE + sizeof(PREFIX) - 1)
-
-#define NUM_OF_CLIENTS 4
 
 #define SWITCH_SEQUENCE "\xa1\x01\x22\x00\x00\x00\x00\x00\x00\x00"
+#define KEY_UP_SEQUENCE "\xa1\x01\x00\x00\x00\x00\x00\x00\x00\x00"
 
-static struct client
+struct _tgot_ctx
 {
-    const char *address;
+    const char **clients;
+    int selected_client;
     int control_socket;
     int interrupt_socket;
-} clients[NUM_OF_CLIENTS];
+    bool connected;
+    pthread_mutex_t *mutex;
+};
 
-static struct client *current_client = NULL;
+typedef struct _tgot_ctx tgot_ctx;
 
 static pthread_mutex_t mutex;
 
-static void switch_current_client()
+static void send_to_client(tgot_ctx *ctx, const char *input, int size)
 {
-    int i, j;
-    // Find current client
-    for (i = 0; i < NUM_OF_CLIENTS; i++)
-    {
-        if (&clients[i] == current_client)
-        {
-            close(clients[i].control_socket);
-            close(clients[i].interrupt_socket);
-            break;
-        }
-    }
-
-    // Find next connected client
-    for (j = (i + 1) % NUM_OF_CLIENTS; j != i; j = (j + 1) % NUM_OF_CLIENTS)
-    {
-        if (clients[j].address)
-        {
-                clients[j].control_socket =
-                    bt_connect_to_psm(clients[j].address, 17);
-
-                if (clients[j].control_socket >= 0)
-                {
-                    printf("Success on control socket for client %d\n", j + 1);
-                }
-                else
-                {
-                    continue;
-                }
-        
-                clients[j].interrupt_socket =
-                    bt_connect_to_psm(clients[j].address, 19);
-
-                if (clients[j].interrupt_socket >= 0)
-                {
-                    printf("Success on interrupt socket for client %d\n",
-                        j + 1);
-
-                    current_client = &clients[j];
-                    break;
-                }
-                else
-                {
-                    continue;
-                }
-        }
-    }
+    pthread_mutex_lock(ctx->mutex);
+    write(ctx->interrupt_socket, input, size);
+    pthread_mutex_unlock(ctx->mutex);
 }
 
-static void send_to_client(const char *input, int size)
+static void close_client_connection(tgot_ctx *ctx)
 {
-    if (current_client != NULL)
+    send_to_client(ctx, KEY_UP_SEQUENCE, sizeof(KEY_UP_SEQUENCE) - 1);
+    close(ctx->control_socket);
+    close(ctx->interrupt_socket);
+    ctx->connected = false;
+}
+
+static void create_client_connection(tgot_ctx *ctx)
+{
+    const char *address = ctx->clients[ctx->selected_client];
+
+    ctx->control_socket = bt_connect_to_psm(address, 17);
+
+    if (ctx->control_socket < 0)
     {
-        pthread_mutex_lock(&mutex);
-        write(current_client->interrupt_socket, input, size);
-        pthread_mutex_unlock(&mutex);
+        return;
     }
+    
+    printf("Success on control socket for client %s\n", address);
+
+    ctx->interrupt_socket = bt_connect_to_psm(address, 19);
+
+    if (ctx->interrupt_socket < 0)
+    {
+        close(ctx->control_socket);
+        return;
+    }
+    
+    printf("Success on interrupt socket for client %s\n", address);
+    ctx->connected = true;
+}
+
+static void switch_current_client(tgot_ctx *ctx)
+{
+    ctx->selected_client++;
+    
+    // Go back to first client if we are at the end of the list
+    if (ctx->clients[ctx->selected_client] == NULL)
+    {
+        ctx->selected_client = 0;
+    }
+    
+    // Close current client
+    if (ctx->connected)
+    {
+        close_client_connection(ctx);
+    }
+
+    // Connect to client
+    create_client_connection(ctx); 
 }
 	
 static void *usb_loop(void *data)
 {
     char buffer[USB_BUFFER_SIZE];
+    tgot_ctx *p_tgot_ctx = (tgot_ctx*)data;
     usb_hid_ctx *ctx = usb_hid_init(0x046d, 0xc52f, 0);
 
 	if (ctx == NULL)
@@ -105,7 +106,7 @@ static void *usb_loop(void *data)
 
     printf("Connected to USB device\n");
 
-    strcpy(buffer, PREFIX);
+    strcpy(buffer, USB_PREFIX);
 
 	for (;;)
 	{
@@ -114,9 +115,9 @@ static void *usb_loop(void *data)
 		if (usb_hid_get_report(ctx, 0x81,
 					input, USB_INPUT_SIZE))
 		{
-			memcpy(buffer + sizeof(PREFIX) - 1, input, USB_INPUT_SIZE);
+			memcpy(buffer + sizeof(USB_PREFIX) - 1, input, USB_INPUT_SIZE);
 
-			send_to_client(buffer, USB_BUFFER_SIZE);
+			send_to_client(p_tgot_ctx, buffer, USB_BUFFER_SIZE);
 		}
 	}
 
@@ -125,6 +126,7 @@ static void *usb_loop(void *data)
 
 static void *bt_loop(void *data)
 {
+    tgot_ctx *p_tgot_ctx = (tgot_ctx*)data;
     bt_hid_ctx *ctx = bt_hid_init();
 
 	if (ctx == NULL)
@@ -143,10 +145,10 @@ static void *bt_loop(void *data)
 		{
             if (strncmp(input, SWITCH_SEQUENCE, bytesRead) == 0)
             {
-                switch_current_client();
+                switch_current_client(p_tgot_ctx);
             }
 
-			send_to_client(input, bytesRead);
+			send_to_client(p_tgot_ctx, input, bytesRead);
 		}
 	}
 
@@ -156,27 +158,22 @@ static void *bt_loop(void *data)
 int main(int argc, char *argv[])
 {
     pthread_t bt_thread, usb_thread;
+    pthread_mutex_t mutex;
 
-    memset(clients, 0, sizeof(clients));
-
-    clients[0].address = CLIENT0_ADDRESS;
-    clients[1].address = CLIENT1_ADDRESS;
-
-    switch_current_client();
+    const char *clients[] = {"60:BE:B5:30:61:AB", "b8:8a:60:6a:68:d6", NULL};
+    tgot_ctx ctx = {clients, -1, -1, -1, false, &mutex};
 
     pthread_mutex_init(&mutex, NULL);
+    
+    switch_current_client(&ctx);
 
-    pthread_create(&bt_thread, NULL, bt_loop, NULL);
-    pthread_create(&usb_thread, NULL, usb_loop, NULL);
+    pthread_create(&bt_thread, NULL, bt_loop, &ctx);
+    pthread_create(&usb_thread, NULL, usb_loop, &ctx);
 
     pthread_join(bt_thread, NULL);
     pthread_join(usb_thread, NULL);
 
-    if (current_client != NULL)
-    {
-        close(current_client->control_socket);
-        close(current_client->interrupt_socket);
-    }	
+    close_client_connection(&ctx);
 
 	return 0;
 }
